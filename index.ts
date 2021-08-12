a6a6590fabcb94da2834b11423085d222453838f
dg12988 <doug.goldberg@me.com>
2021-08-12 16:12:20 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 //swamps http end point dynamically based on user input
 
+import { fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { concatMap, map } from "rxjs/operators";
+
 const endpointInput: HTMLInputElement = document.querySelector('input#endpoint');
 const fetchButton = document.querySelector('button#fetch');
+
+fromEvent(fetchButton, 'click').pipe(
+  map(() => endpointInput.value),
+  concatMap(value => 
+    ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+  )
+).subscribe(
+  value => console.log(value)
+);
 
