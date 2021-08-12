719252a08f3fe839af0cd2f0e58ed60138f1cceb
dg12988 <doug.goldberg@me.com>
2021-08-12 16:04:40 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+//swamps http end point dynamically based on user input
+
 const endpointInput: HTMLInputElement = document.querySelector('input#endpoint');
 const fetchButton = document.querySelector('button#fetch');
 
